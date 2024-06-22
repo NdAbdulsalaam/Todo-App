3b4d08ce712e9be6142ec8ef4a2f1367761811ee
Nurudeen Abdulsalaam <olaitansalaam2012@gmail.com>
2024-06-22 11:05:06 +0100

    include my name as the author

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
         <header className={styles.header}>
             <h1>Todos</h1>
             <p>Items will be stored on the browser's local storage</p>
+            <p>By: <a href="https://github.com/NdAbdulsalaam" target="blanck">Nurudeen Abdulsalaam</a></p>
         </header>
     );
     };
